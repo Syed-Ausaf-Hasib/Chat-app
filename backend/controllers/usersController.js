42b6f0c7867f71320717ccf92f6774280a3334c5
Syed Ausaf Hasib <syedausaf2003@gmail.com>
2024-06-30 17:39:16 +0530

    bcryptjs

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,5 +1,6 @@
 const User = require("../model/userModel")
-const bcrypt = require("bcrypt")
+// const bcrypt = require("bcrypt")
+const bcrypt = require('bcryptjs');
 
 module.exports.register = async (req, res, next) => {
     try {
