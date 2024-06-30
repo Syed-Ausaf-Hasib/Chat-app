d3ed63e0d1fc00db3eb4d145b278ef73c6d4479b
Syed Ausaf Hasib <syedausaf2003@gmail.com>
2024-06-30 18:20:00 +0530

    remove local host

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://chat-app-8h57.onrender.com",
         credentials: true,
     }
 })
