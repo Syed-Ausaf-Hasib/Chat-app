da1f37821fd47dfe6500ae00937475eaa4679ac5
Syed Ausaf Hasib <syedausaf2003@gmail.com>
2024-06-30 16:57:36 +0530

    Final

--- backend/index.js
+++ backend/index.js
@@ -18,18 +18,18 @@ app.use("/api/messages",messageRoute)
 
 //============================DEPLOYMENT====================
 
-// const __dirname1 = path.resolve()
-// if(process.env.NODE_ENV==='production'){
-//     app.use(express.static(path.join(__dirname1,'/frontend/build')))
-//     app.get("*",(req,res)=>{
-//         res.send
-//     })
-// }
-// else{
-//     app.get("/",(req,res)=>{
-//         res.send("API is running...")
-//     })
-// }
+const __dirname1 = path.resolve()
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,'/frontend/build')))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","indeex.html"))
+    })
+}
+else{
+    app.get("/",(req,res)=>{
+        res.send("API is running...")
+    })
+}
 
 //============================DEPLOYMENT====================
 
